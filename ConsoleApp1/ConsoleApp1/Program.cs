81f28c0fe66baad2f84c924020cb82f1561ea95c
vipinit <vipinit@gmail.com>
2017-09-07 10:57:07 +0100

    wwd

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("new");
+            Console.WriteLine("new1");
         }
     }
 }
