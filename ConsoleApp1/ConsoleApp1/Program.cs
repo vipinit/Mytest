9fbd5470d937cf902398a4fa73ea15e204e677b9
vipinit <vipinit@gmail.com>
2017-09-07 11:02:54 +0100

    fret

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("new1");
+            Console.WriteLine("new2");
         }
     }
 }
