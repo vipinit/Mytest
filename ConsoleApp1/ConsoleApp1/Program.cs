59c1792f0ed418f399ecf59e38d13aeb2db03a20
vipinit <vipinit@gmail.com>
2017-09-12 11:37:55 +0100

    commiyted

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("new5");
+            Console.WriteLine("new6");
         }
     }
 }
